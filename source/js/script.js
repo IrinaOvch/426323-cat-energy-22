ce66bec7e8d3969059f955ecfeb063b8778a4a29
Irina Ovchinnikova <iovchinnikova@outlook.com>
2021-05-16 21:29:23 +0300

    Combine all js files in one

--- source/js/script.js
+++ source/js/script.js
@@ -1,3 +1,21 @@
+const navigationToggle = document.querySelector(".main-navigation__toggle");
+const navigationLinks = document.querySelector(".main-navigation__links");
+
+navigationToggle.classList.add("main-navigation__toggle--visible");
+navigationToggle.classList.remove("main-navigation__toggle--opened");
+navigationLinks.classList.add("main-navigation__links--closed");
+
+navigationToggle.addEventListener("click", toggleNavigationMenu);
+
+function toggleNavigationMenu(evt) {
+  evt.preventDefault();
+  navigationToggle.classList.toggle("main-navigation__toggle--opened");
+  navigationLinks.classList.toggle("main-navigation__links--closed");
+}
+
+
+
+
 const toggle = document.querySelector(".toggle__scale-toggle");
 const beforeButton = document.querySelector(".toggle__button--before");
 const afterButton = document.querySelector(".toggle__button--after");
